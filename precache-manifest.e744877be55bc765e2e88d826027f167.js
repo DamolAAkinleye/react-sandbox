2467e5508ba85c079b16aaa2e9f756295f3ddaae
Damola <persistence911@gmail.com>
2020-06-11 16:59:08 +0100

    Updates

---
+++ precache-manifest.e744877be55bc765e2e88d826027f167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad4e0d8990f8628f954fda8a0d35ca4",
+    "url": "/react-sandbox/index.html"
+  },
+  {
+    "revision": "8b7fe9bb3a24081e0a0a",
+    "url": "/react-sandbox/static/css/main.8bbc0a1d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-sandbox/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sandbox/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7fe9bb3a24081e0a0a",
+    "url": "/react-sandbox/static/js/main.89879854.chunk.js"
+  },
+  {
+    "revision": "022d55528f565ddb8097",
+    "url": "/react-sandbox/static/js/runtime-main.42d92bf7.js"
+  }
+]);
